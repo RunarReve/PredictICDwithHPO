15ed800eb2b16c621cddf8ce0138f8d0fe3c019e
RunarReve <reverunar@outlook.com>
2020-03-09 23:48:04 +0300

    Added "save removed ICDs" to random remover

--- randRmMor.py
+++ randRmMor.py
@@ -10,21 +10,25 @@ def main():
    dc=0
    c =0
    outfile = open(sys.argv[2], 'w')
+   outrem  =  open(sys.argv[2]+'.rem', 'w')
    for line in open(sys.argv[1], 'r'):
      sline = line.split()
      for each in sline:
         if (each == sline[0]):
            out = [each]
+           rem = [each]
            continue 
         c += 1
         #if random hit the wanted value, then remove it
         if(randint(1,10) == 1): 
            dc += 1 
+           rem.append(each)
         else:
            out.append(each)
      
      #print(randint(0,10))
      outfile.write("\t".join(out) + '\n')
+     outrem.write("\t".join(rem) + '\n')
    print(dc/c)
 
 if __name__ == "__main__":
